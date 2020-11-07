17bea9aaa4fb6ff8677d0de4267443887c1db404
Denis Mashkov <mashkov@gmail.com>
2020-11-07 15:41:45 +0300

    added port & controllers

--- index.js
+++ index.js
@@ -99,10 +99,72 @@ dis.dispatch('test', {a: 1, b: 2})
 // http
 const http = require('http')
 const server = http.createServer((request, response) => {
-    response.end('<h1>This is the end, beautiful friend</h1><h2>This is the end, my only friend</h2>')
+
+    // if (request.url === '/') {
+    //     fs.readFile(path.join(__dirname, 'public', 'index.html'), (error, data) => {
+    //         if (error) {
+    //             throw error;
+    //         }
+    //         response.writeHead(200, {
+    //             'Content-Type': 'text/html',
+    //         })
+    //         response.end(data)
+    //     })
+    // } else if (request.url === '/contact') {
+    //     fs.readFile(path.join(__dirname, 'public', 'contact.html'), (error, data) => {
+    //         if (error) {
+    //             throw error;
+    //         }
+    //         response.writeHead(200, {
+    //             'Content-Type': 'text/html',
+    //         })
+    //         response.end(data)
+    //     })
+    // }
+
+    let filePath = path.join(__dirname, 'public', request.url === '/' ? 'index.html' : request.url)
+    const ext = path.extname(filePath)
+    let contentType = 'text/html'
+    switch (ext) {
+        case '.css':
+            contentType = 'text/css'
+            break
+        case '.js':
+            contentType = 'text/javascript'
+            break
+    }
+
+    if (!ext) {
+        filePath += '.html'
+    }
+    fs.readFile(filePath, (error, content) => {
+        if (error) {
+            fs.readFile(path.join(__dirname, 'public', 'error.html'), (error, data) => {
+                if (error) {
+                    response.writeHead(500)
+                    response.end('Error')
+                } else {
+                    response.writeHead(200, {
+                        'Content-Type': 'text/html'
+                    })
+                    response.end(data)
+                }
+
+
+            })
+        } else {
+            response.writeHead(200, {
+                'Content-Type': contentType
+            })
+            response.end(content)
+        }
+
+    })
+
 })
 
-server.listen(3000, () => {
-    console.log('server has been started...')
+const PORT = process.env.PORT || 3000
+server.listen(PORT, () => {
+    console.log(`server has been started on ${PORT}...`)
 })
 
